8065887a5bc4a2f7ea5fd498b61d4a72266f858c
indratdev <indrat.dev@gmail.com>
2024-06-13 16:21:34 +0700

    fixing code screen detail surah

--- lib/presentation/screens/surah_detail/widgets/export.dart
+++ lib/presentation/screens/surah_detail/widgets/export.dart
@@ -1,3 +1,6 @@
 export 'package:kita_muslim/presentation/screens/surah_detail/widgets/bismillah_widget.dart';
 export 'package:kita_muslim/presentation/widgets/customwidgets.dart';
 export 'package:kita_muslim/presentation/widgets/icon_number_arabic_widget.dart';
+export 'package:kita_muslim/presentation/screens/surah_detail/widgets/audio_widget.dart';
+export 'package:kita_muslim/presentation/screens/surah_detail/widgets/favorite_widget.dart';
+export 'package:kita_muslim/presentation/screens/surah_detail/widgets/scroll_down_surah_widget.dart';

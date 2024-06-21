b9be9ce3058e4899a5dfcc8071506bd902fe43dd
indratdev <indrat.dev@gmail.com>
2024-06-21 10:38:14 +0700

    fixing accordion daily prayer

--- lib/presentation/screens/surah_harian/doaharianscreen.dart
+++ lib/presentation/screens/surah_harian/doaharianscreen.dart
@@ -2,18 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:kita_muslim/presentation/screens/surah_detail/widgets/export.dart';
 import 'package:kita_muslim/presentation/screens/surah_harian/expansion_daily_prayer_widget.dart';
 import 'package:kita_muslim/presentation/widgets/customwidgets.dart';
-import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 import '../../../blocs/export.dart';
 import '../../../data/models/daily_prayer/daily_prayer_model.dart';
-import '../../../utils/constants.dart';
 
 /// https://doa-doa-api-ahmadramadhan.fly.dev/api
 
 class DoaHarianScreen extends StatelessWidget {
-  final ItemScrollController _itemScrollController = ItemScrollController();
-
-  DoaHarianScreen({Key? key}) : super(key: key);
+  const DoaHarianScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -33,62 +29,7 @@ class DoaHarianScreen extends StatelessWidget {
           }
           if (state is SuccessGetSurahHarian) {
             List<DailyPrayerModel> result = state.surah;
-            return ScrollablePositionedList.builder(
-              itemScrollController: _itemScrollController,
-              initialScrollIndex: state.indexSurah,
-              itemCount: result.length,
-              itemBuilder: (context, index) {
-                return ExpansionDailyPrayerWidget(data: result);
-
-                // return InkWell(
-                //   onTap: () {
-
-                //     Navigator.pushNamed(context, '/doahariandetail');
-                //   },
-                //   child: ListTile(
-                //     minLeadingWidth: 10,
-                //     leading: SizedBox(
-                //       width: MediaQuery.sizeOf(context).width / 10,
-                //       child: IconNumberArabicWidget(number: index + 1),
-                //     ),
-                //     title: Text(
-                //       data.doa,
-                //       style: const TextStyle(fontSize: Constants.sizeTextTitle),
-                //     ),
-                //     // subtitle: Text(
-                //     //     '${data[index].translation_id} (${data[index].number_of_verses} Ayat) ',
-                //     //     maxLines: 2,
-                //     //     style: const TextStyle(
-                //     //       fontSize: Constants.sizeText,
-                //     //     ),
-                //     //     overflow: TextOverflow.ellipsis),
-                //     // trailing: Text(
-                //     //   data[index].name_short.toString(),
-                //     //   style: const TextStyle(
-                //     //       fontSize: Constants.sizeTextArabian,
-                //     //       fontWeight: FontWeight.bold),
-                //     // ),
-                //   ),
-                //   // ListTile(
-                //   //   minLeadingWidth: 10,
-                //   //   leading: Text(
-                //   //     "${index + 1}.",
-                //   //     style: const TextStyle(
-                //   //       fontSize: Constants.sizeSubTextTitle,
-                //   //       overflow: TextOverflow.clip,
-                //   //     ),
-                //   //   ),
-                //   //   title: Text(
-                //   //     result[index].doa.toString(),
-                //   //     style: const TextStyle(
-                //   //       fontSize: Constants.sizeSubTextTitle,
-                //   //       overflow: TextOverflow.clip,
-                //   //     ),
-                //   //   ),
-                //   // ),
-                // );
-              },
-            );
+            return ExpansionDailyPrayerWidget(data: result);
           } else {
             return Container();
           }
--- lib/presentation/screens/surah_harian/expansion_daily_prayer_widget.dart
+++ lib/presentation/screens/surah_harian/expansion_daily_prayer_widget.dart
@@ -1,73 +1,13 @@
 import 'package:flutter/material.dart';
 
 import '../../../data/models/daily_prayer/daily_prayer_model.dart';
-
-// class ExpansionDailyPrayerWidget extends StatefulWidget {
-//   List<DailyPrayerModel> data;
-
-//   ExpansionDailyPrayerWidget({
-//     super.key,
-//     required this.data,
-//   });
-
-//   @override
-//   State<ExpansionDailyPrayerWidget> createState() =>
-//       _ExpansionDailyPrayerWidgetState();
-// }
-
-// class _ExpansionDailyPrayerWidgetState
-//     extends State<ExpansionDailyPrayerWidget> {
-//   // final List<Item> _data = generateItems(8);
-
-//   late List<ExpandableDailyPrayerModel> _data;
-
-//   @override
-//   void initState() {
-//     _data = widget.data
-//         .map((dailyPrayer) =>
-//             ExpandableDailyPrayerModel(dailyPrayer: dailyPrayer))
-//         .toList();
-//     super.initState();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return SingleChildScrollView(
-//       child: ExpansionPanelList(
-//         expansionCallback: (int index, bool isExpanded) {
-//           setState(() {
-//             _data[index].isExpanded = !isExpanded;
-//           });
-//         },
-//         children: _data.map<ExpansionPanel>((ExpandableDailyPrayerModel item) {
-//           return ExpansionPanel(
-//             headerBuilder: (BuildContext context, bool isExpanded) {
-//               return ListTile(
-//                 title: Text(item.dailyPrayer.doa),
-//               );
-//             },
-//             body: ListTile(
-//               title: Column(
-//                 crossAxisAlignment: CrossAxisAlignment.start,
-//                 children: [
-//                   Text('Ayat: ${item.dailyPrayer.ayat}'),
-//                   Text('Latin: ${item.dailyPrayer.latin}'),
-//                   Text('Artinya: ${item.dailyPrayer.artinya}'),
-//                 ],
-//               ),
-//             ),
-//             isExpanded: item.isExpanded,
-//           );
-//         }).toList(),
-//       ),
-//     );
-//   }
-// }
+import '../../../utils/constants.dart';
+import '../surah_detail/widgets/export.dart';
 
 class ExpansionDailyPrayerWidget extends StatefulWidget {
   final List<DailyPrayerModel> data;
 
-  ExpansionDailyPrayerWidget({
+  const ExpansionDailyPrayerWidget({
     super.key,
     required this.data,
   });
@@ -87,6 +27,7 @@ class _ExpansionDailyPrayerWidgetState
         .map((dailyPrayer) =>
             ExpandableDailyPrayerModel(dailyPrayer: dailyPrayer))
         .toList();
+
     super.initState();
   }
 
@@ -99,10 +40,14 @@ class _ExpansionDailyPrayerWidgetState
             _data[index].isExpanded = isExpanded;
           });
         },
-        children: _data.map<ExpansionPanel>((ExpandableDailyPrayerModel item) {
+        children: _data.map((ExpandableDailyPrayerModel item) {
+          final index = _data.indexOf(item);
           return ExpansionPanel(
+            canTapOnHeader: true,
             headerBuilder: (BuildContext context, bool isExpanded) {
               return ListTile(
+                contentPadding: const EdgeInsets.all(12),
+                leading: IconNumberArabicWidget(number: index + 1),
                 title: Text(item.dailyPrayer.doa),
               );
             },
@@ -110,9 +55,34 @@ class _ExpansionDailyPrayerWidgetState
               title: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text('Ayat: ${item.dailyPrayer.ayat}'),
-                  Text('Latin: ${item.dailyPrayer.latin}'),
-                  Text('Artinya: ${item.dailyPrayer.artinya}'),
+                  Directionality(
+                    textDirection: TextDirection.rtl,
+                    child: Text(
+                      item.dailyPrayer.ayat,
+                      textAlign: TextAlign.right,
+                      style: const TextStyle(
+                        fontSize: Constants.sizeTextArabian,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 14),
+                  Text(
+                    item.dailyPrayer.latin,
+                    textAlign: TextAlign.justify,
+                    style: const TextStyle(
+                      fontSize: Constants.sizeSubTextTitle,
+                      color: Constants.deepGreenColor,
+                    ),
+                  ),
+                  const SizedBox(height: 14),
+                  Text(
+                    item.dailyPrayer.artinya,
+                    textAlign: TextAlign.justify,
+                    style: const TextStyle(
+                      fontSize: Constants.sizeSubTextTitle,
+                      color: Constants.blackColor,
+                    ),
+                  ),
                 ],
               ),
             ),
